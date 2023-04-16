979e4a03e51d952c7f302b869da6117d1cb3aeed
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-04-16 19:23:03 +0100

    adapted way of increasing quadrature points

--- General_functions/test_midpoint_dphikdn.m
+++ General_functions/test_midpoint_dphikdn.m
@@ -1,4 +1,5 @@
 % testing midpoint_dphikdn_f_diff_screen.m
+clear all
 
 a = 0;
 b = 2*pi;
@@ -7,20 +8,24 @@ k = 10;
 
 s = pi + 0.1;
 
-Q = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768];
-
+% Q = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768];
+Q_max = 20;
 % test f functions
 f = @(t) 1; 
-% f = @(t) sin(k*t);
-% f = @(t) exp(1i*k*t);
+f = @(t) sin(k*t);
+f = @(t) exp(1i*k*t);
 
 x1 = -1; %[-1, -4, -0.1];
 x2 = 1; %[-1, -4, -0.1];
 
 n = [1/sqrt(2), 1/sqrt(2)];
 
-for q = 1:length(Q)
-    h = b/(Q(q) + 1);
+for q = 1:Q_max
+% for q = 1:length(Q)
+%     h = b/(Q(q) + 1);
+
+    Q = 2^q;
+    h = b/(Q + 1);
     nq = [a + h/2: h:b - h/2];
 
     y1t = nq;
@@ -32,7 +37,7 @@ for q = 1:length(Q)
 
 end
 
-Q_large = Q(end)*10;
+Q_large = (2^20)*10;
 h = b/(Q_large + 1);
 nq = [a + h/2: h:b - h/2];
 y1t = nq;
@@ -46,7 +51,7 @@ err = I_large_Q - I;
 
 % convergence
 
-for q = 1:(length(Q) - 1)
+for q = 1:(length(I) - 1)
 
     EOC(q) = log2( err(q)/err(q+1));
 
@@ -56,3 +61,5 @@ end
 err.'
     
 EOC.' 
+
+I.'
