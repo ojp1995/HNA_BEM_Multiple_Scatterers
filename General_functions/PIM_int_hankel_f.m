e258f04919b29d00cb5ac979c61a7b7386eac8fd
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-04-18 08:52:26 +0100

    corrects so storing for each collocation point

--- General_functions/PIM_int_hankel_f.m
+++ General_functions/PIM_int_hankel_f.m
@@ -11,13 +11,13 @@ function I = PIM_int_hankel_f(k, s, h, nq, fnq, tq, C1, C2)
 t_lower = tq(1:end - 1);
 t_upper = tq(2:end);
 
-I = 0;
+% I = 0;
 
 for j = 1:length(s)
 
     dist = abs(s(j) - nq);
 
-    I = I + sum( -smoothing_function(nq, C1, C2).*besselj(0, k*dist).*fnq.*w1_weights(k, s(j), t_lower, t_upper)/(2*pi) ...
+    I(j, 1) = sum( -smoothing_function(nq, C1, C2).*besselj(0, k*dist).*fnq.*w1_weights(k, s(j), t_lower, t_upper)/(2*pi) ...
         +  h*fnq.*( 1i*besselh(0, k*dist)/4 + smoothing_function(nq, C1, C2).*besselj(0, k*dist).*log(k*dist)/(2*pi) ) );
 
    
