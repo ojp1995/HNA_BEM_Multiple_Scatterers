27aea6e5088469fc29dde713394c672c4aae9c8c
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-05-11 17:58:08 +0100

    adds correct integration nodes with more detailed var names

--- General_functions/compute_RHS_vec_given_coll_vec.m
+++ General_functions/compute_RHS_vec_given_coll_vec.m
@@ -1,42 +1,56 @@
-function f_ell = compute_RHS_vec_given_coll_vec(Gamma_ell, L_ell, k, ...
-    d, s, x1s, x2s, nq, y1nq, y2nq, h_outer, phi_j_outer, tq, C1, C2, ...
-    h_inner, y1nq_inner, y2nq_inner, phi_j_inner, nj , theta)
+function f_ell = compute_RHS_vec_given_coll_vec(Gamma_ell, L_ell, k, d, ...
+    theta, norm_j, s_ell, x1s_ell, x2s_ell, C1, C2, y1nq_j, y2nq_j, ...
+    h_j_outer, phi_j_outer, y1nq_ell_PIM, y2nq_ell_PIM, nq_ell_PIM, ...
+    tq_ell_PIM, h_ell_outer_PIM, y1nq_j_inner, y2nq_j_inner, h_j_inner, ...
+    phi_j_inner)
 % In this function ...
 %
-% Inputs:
-% Gamma_ell, is the coordinates of the Gamma_ell
-% L_ell is the length of Gamma_ell
-% k, the wavenumber
-% s, 1D parameterised collocation points
-% d is the directional unit vector of the incident wave
-% (x1s, x2s), collocation points evaluated in 2D
-% nq, paramterised integration nodes, outer integral
-% (y1nq, y2nq), integration nodes in 2D
-% h_outer, integration weight for outer integral
-% phi_j_outer, phi_j evaluated at outer quadrature nodes.
-% tq, mesh for integration of outer integral (PIM)
-% C1, C2, constants needed for smoothing 
-% 
-% (y1nq, y2nq), integration nodes in 2D for outer integral
-% h_inner, integration weights for inner integral
-% (y1nq_inner, y2nq_inner), integration nodes for inner integral
-% phi_j_inner, phi_j elvaluated at inner quadrature nodes, vector.
-% nj, unit normal for Gamma_j.
-% theta, the angle of the incident wave measured from the downwards
+% Problem parameters:
+% Gamma_ell, set up of the screen Gamma_ell
+% L_ell, the length of the screen Gamma_ell
+% k the wave number
+% d the unit direction of the incident wave
+% theta the angle of the incident wave measured from the downwards
 % vertical anti-clockwise 
-
+% norm_j, unit normal of Gamma_j
+% s_ell, parameterised collocation points in 1D
+% (x1s_ell, x2s_ell), Collocation points in 2D
+% C1, C2, constants needed for PIM method
+%
+% Discretisation parameters:
+% 1. Integration variables for approximating outer integral over Gamma_j:
+%     (y1nq_j, y2nq_j), integration nodes for this integral
+%     h_j_outer, integration weights for this integral
+%     phi_j_outer, vector phi_j evlauated at the integration nodes
+%
+% 2. Integration variables for approximating the integral over Gamma_ell
+% using the product integration method
+%     (y1nq_ell_PIM, y2nq_ell_PIM), integration nodes for this integral
+%     nq_ell_PIM, parameterised 1D integration nodes for this integral
+%     tq_ell_PIM, discretisation on Gamma_ell for integration
+%     h_ell_outer_PIM, intergation weight
+%
+% 3. Integration variables for approximating the integral Psi_ell_r:
+%     (y1nq_j_inner, y2nq_j_inner), intergation nodes in 2D
+%     h_j_inner, integration weights
+%     phi_j_inner, phi_j evaluated at the integration nodes
+%
 % Outputs:
 % f_ell, right hand side evaluated at the collocation points, s.
 
 % First we will compute the leading order behaviour where the collocation
 % points we are evaluating the integral are the quadrature nodes for the
 % outer integral.
-Psi_ell_r = 2*duidn(Gamma_ell, L_ell, k, d, nq) ...
-    - 2*midpoint_dphikdn_f_diff_screen(k, y1nq, y2nq, h_inner, ...
-    y1nq_inner, y2nq_inner, phi_j_inner, nj);
+Psi_ell_r = 2*duidn(Gamma_ell, L_ell, k, d, nq_ell) ...
+    - 2*midpoint_dphikdn_f_diff_screen(k, y1nq_ell_PIM, y2nq_ell_PIM, ...
+    h_j_inner, y1nq_j_inner, y2nq_j_inner, phi_j_inner, norm_j);
 
 % now we will compute the whole vector
-f_ell = incident(k, theta, x1s, x2s) ...
-    - midpoint_hankel_f_diff_screen(k, x1s, x2s, y1nq, y2nq, h_outer, ...
-    phi_j_outer) - PIM_int_hankel_f(k, s, h_outer, nq, Psi_ell_r, tq, ...
-    C1, C2); 
+f_ell = incident(k, theta, x1s_ell, x2s_ell) ...
+    - midpoint_hankel_f_diff_screen(k, x1s_ell, x2s_ell, y1nq_j, ...
+    y2nq_j, h_j_outer, phi_j_outer) ...
+    - PIM_int_hankel_f(k, s_ell, h_ell_outer_PIM, nq_ell_PIM, ...
+    Psi_ell_r, tq_ell_PIM, C1, C2); 
+
+end
+
