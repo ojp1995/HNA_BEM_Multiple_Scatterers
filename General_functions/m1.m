82908c53c4644a354d3412ac9a7a2c8db249e733
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-05-25 18:52:42 +0100

    kernels approximating in more modular way, m2 not sure on

---
+++ General_functions/m1.m
@@ -0,0 +1,19 @@
+function z = m1(k, s, nq, C1, C2)
+% In this function we will be approximating the kernel of integral of the
+% hankel function, specifically, 
+%       m1(k, s, nq) = - chi(nq, C1, C2)J_0(k|s - nq|)/2pi,
+% where chi(t, C1, C2) is the smoothing function and J_0 is the Bessel
+% function of the first kind.
+% 
+% Inputs:
+% k, the wavenumber
+% s, the collocation points we are evluating the integral at
+% nq, the quadrature nodes
+% C1, C2 are constants for the smoothing function
+%
+% Outputs:
+% z, approximation of m1
+
+z = -smoothing_function(nq, C1, C2).*besselj(0, k*abs(s - nq))/(2*pi);
+
+end
--- General_functions/m2.m
+++ General_functions/m2.m
@@ -1,6 +1,6 @@
 function z = m2(k, s, nq, C1, C2)
 % In this function we will be computing the following kernel:
-% m2(s, nq) = i H_{0}^{(1)}(k | s - nq |) - \sigma_{1}(s, t)m_{1}(s, t)
+% m2(s, nq) = i H_0^(1)(k |s - nq|)/4 - \sigma_1(s, t)m_{1}(s, t)
 
 % select = (s ~= nq);
 
@@ -9,7 +9,7 @@ select = (s == nq);
 dist = s - nq;
 
 z = 1i*besselh(0, k*dist)/4 ...
-    + smoothing_function(nq, C1, C2).*besselj(0, k*dist).*log(k*dist)/(2*pi); 
+    + m1(k, s, nq, C1, C2).*log(k*dist)/(2*pi); %% should this be w1_weights??
 
-z(select) = 1i/4 - 2*log(1/2)/pi;
+z(select) = 1i/4 - 2*(log(1/2) - (-psi(1)))/pi;
 
