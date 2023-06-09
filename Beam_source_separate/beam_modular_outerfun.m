1fbd968637f79c4ce35a78454436edeb58dadb10
ojp1995 <43139955+ojp1995@users.noreply.github.com>
2023-06-09 17:25:02 +0100

    creates modular beam approach

---
+++ Beam_source_separate/beam_modular_outerfun.m
@@ -0,0 +1,144 @@
+% In this function we will be looking at the effect of the beam source only
+% and making a few adaptations to some of the current general functions.
+% This will be largely similar to the modular_outer_fun.m it will change
+% when we get to Step 1.
+
+
+clear all
+clear classes
+
+addpath('/Users/ojp18/Dropbox/Mac/Documents/GitHub/HNA_BEM_Multiple_Scatterers/General_functions')
+
+vertices1 = [-2*pi 2*pi;
+    0, 0];
+Gamma1=Screen(vertices1);
+
+vertices2 = [2*pi 0;
+    5*pi 3*pi];
+            
+Gamma2=Screen(vertices2);
+
+% General set up
+%wavenumber
+kwave=10;
+theta = 0;
+% theta = pi/4; %% need to change so it is consistent
+d = [sin(theta) -cos(theta) ];
+uinc=planeWave(kwave,d);
+
+pMax = 4; %polynomial degree
+cL = 2; %layers of grading per polynomial degree
+sigmaGrad=0.15; %grading ratio
+nLayers = cL*(pMax+1)-1; %number of layers of grading
+throwAwayParam = 0; %no need to remove any basis elements
+OverSample = 1.25; %choose amount to oversample by (40% here)
+
+[v_N1, GOA1, colMatrix1, colRHS1, col_points1,...
+v_N2, GOA2, colMatrix2, colRHS2, col_points2, VHNA1, VHNA2] ...
+    = AG_code_pulling_out_info(pMax, cL, sigmaGrad, nLayers, OverSample, ...
+    Gamma1, Gamma2, kwave, uinc );
+
+% Info needed for our solve
+C1 = 1;
+C2 = pi;
+% Isolating the collocation points in cartesian coordinates
+L1 = sqrt( (vertices1(2, 1) - vertices1(1, 1))^2 + (vertices1(2, 2) - vertices1(1, 2))^2 );
+x1_col = vertices1(1, 1) + col_points1*(vertices1(2, 1) - vertices1(1, 1))/L1;
+y1_col = vertices1(1, 2) + col_points1*( vertices1(2, 2) - vertices1(1, 2) )/L1;
+
+L2 = sqrt( (vertices2(2, 1) - vertices2(1, 1))^2 + (vertices2(2, 2) - vertices2(1, 2))^2 );
+x2_col = vertices2(1, 1) + col_points2*(vertices2(2, 1) - vertices2(1, 1))/L2;
+y2_col = vertices2(1, 2) + col_points2*( vertices2(2, 2) - vertices2(1, 2) )/L2;
+
+G1 = [vertices1(1, 1), vertices1(1, 2), vertices1(2, 1), vertices1(2, 2)];
+G2 = [vertices2(1, 1), vertices2(1, 2), vertices2(2, 1), vertices2(2, 2)];
+
+n1 = [-(G1(4) - G1(2)), G1(3) - G1(1)]/L1;
+n2 = [-(G2(4) - G2(2)), G2(3) - G2(1)]/L2;
+
+N_approx = 2^(-10);
+[x1, y1, t1, t1_mid, h1, h1vector, N1, L1] = discretisation_variables(G1, N_approx, kwave);
+[x2, y2, t2, t2_mid, h2, h2vector, N2, L2] = discretisation_variables(G2, N_approx, kwave);
+
+% Infor needed for plotting
+x1_plot_1D = [t1_mid(1): 0.001: t1_mid(end)];
+x1_plot = linspace(x1_col(1), x1_col(end), length(x1_plot_1D)).';
+y1_plot = linspace(y1_col(1), y1_col(end), length(x1_plot_1D)).'; 
+
+x2_plot_1D = [t2_mid(1): 0.001: t2_mid(end)];
+
+% x2_plot_1D = linspace(t2_mid(1), t2_mid(1), 1e4).';
+x2_plot = linspace(x2_col(1), x2_col(end), length(x2_plot_1D)).';
+y2_plot = linspace(y2_col(1), y2_col(end), length(x2_plot_1D)).';
+
+%%
+% Step 0
+[v_N_G1_r0] = multiple_Scattering_2screen_step0(kwave, theta, ...
+    d, vertices1, L1, col_points1, x1_col, y1_col, h1, t1_mid, t1, colMatrix1, VHNA1, C1, C2);
+
+phi1_0 = @(x) v_N_G1_r0.eval(x, 1) + 2*duidn(vertices1, L1, kwave, d, x);
+
+% phi1_0 = v_N_G1_r0.eval(x1_plot_1D.', 1) + 2*duidn(vertices1, L1, kwave, d, x1_plot_1D.');
+figure()
+plot(x1_plot_1D/L1, real(phi1_0(x1_plot_1D.')), 'DisplayName', 'OP code', 'LineStyle', '--')
+hold on
+plot(x1_plot_1D/L1, real(v_N1.eval(x1_plot_1D.',1) + GOA1.eval(x1_plot_1D.', 1)), 'DisplayName', 'AG code', 'LineStyle', '-.')
+legend show
+xlim([-0.05 1.05])
+title('Soluiton on the boundary for r=0, screens in line, long way away')
+
+%% Step 1
+N_min = 1;
+N_max = 10;
+for j = N_min:N_max
+    disp(j)
+    N_approx = 2^(-j);
+    [x1, y1, t1, t1_mid, h1, h1vector, N1, L1] = discretisation_variables(G1, N_approx, kwave);
+    [x2, y2, t2, t2_mid, h2, h2vector, N2, L2] = discretisation_variables(G2, N_approx, kwave);
+
+
+    N_approx_inner = N_approx/2;
+    [y1nq_1_inner, y2nq_1_inner, ~, t1_mid_inner, h1_inner, ~, ~, ~] =  discretisation_variables(G1, N_approx_inner, kwave);
+
+% This all needs to be wrapped up in an outer function ideally
+% in this case ell = 2, j = 1
+    phi_1_outer = phi1_0(t1_mid.');
+    phi_1_inner = phi1_0(t1_mid_inner.');
+    
+    [f_2_1(j, :), f_2_1_uinc(j, :), f_2_1_beam(j, :),...
+        ~,S21_phi1_0(j, :), LoB_uinc(j, :), ...
+        LoB_beam(j, :),~, ~] ...
+    = beam_compute_RHS_vec_given_coll_vec(vertices2, L2, kwave, d, ...
+        theta, n1, col_points2, x2_col, y2_col, C1, C2, x1, y1, ...
+        h1, phi_1_outer, x2, y2, t2_mid, ...
+        t2, h2, y1nq_1_inner, y2nq_1_inner, h1_inner, ...
+        phi_1_inner);
+end
+
+%% Looking at the beam part of the solution
+figure()
+for j = N_min:N_max
+    plot(col_points2/L2, real(f_2_1_beam(j, :)))
+    hold on
+    
+end
+title('Beam source, RHs vec')
+
+figure()
+for j = N_min:N_max
+    plot(col_points2/L2, real(f_2_1(j, :)))
+    hold on
+    
+end
+title('RHS vec combined')
+%%
+
+v_N_G2_r1 = compute_coeffs_given_A_and_f(colMatrix2, f_2_1(end, :).', VHNA2);
+
+phi_2_r1 = get_phi_j_r(v_N_G2_r1, vertices2, L2, kwave, d, h1, x1, ...
+    y1, n2, x2_plot_1D, x2_plot, y2_plot, phi1_0(t1_mid.'));
+
+figure()
+plot(x2_plot_1D/L2, real( phi_2_r1 ))
+title('Approximation of $\phi_{2}^{(1)}$')
+
---
+++ General_functions/beam_compute_RHS_vec_given_coll_vec.m
@@ -0,0 +1,72 @@
+function [f_ell, f_ell_uinc, f_ell_beam, ddn_S_ell_j_phij,S_ell_j_phi_j,...
+    LoB_uinc, LoB_beam, LoB_kernel_uinc, LoB_kernel_beam] ...
+    = beam_compute_RHS_vec_given_coll_vec(Gamma_ell, L_ell, k, d, ...
+    theta, norm_j, s_ell, x1s_ell, x2s_ell, C1, C2, y1nq_j, y2nq_j, ...
+    h_j_outer, phi_j_outer, y1nq_ell_PIM, y2nq_ell_PIM, nq_ell_PIM, ...
+    tq_ell_PIM, h_ell_outer_PIM, y1nq_j_inner, y2nq_j_inner, h_j_inner, ...
+    phi_j_inner)
+% In this function ...
+%
+% Problem parameters:
+% Gamma_ell, set up of the screen Gamma_ell
+% L_ell, the length of the screen Gamma_ell
+% k the wave number
+% d the unit direction of the incident wave
+% theta the angle of the incident wave measured from the downwards
+% vertical anti-clockwise 
+% norm_j, unit normal of Gamma_j
+% s_ell, parameterised collocation points in 1D
+% (x1s_ell, x2s_ell), Collocation points in 2D
+% C1, C2, constants needed for PIM method
+%
+% Discretisation parameters:
+% 1. Integration variables for approximating outer integral over Gamma_j:
+%     (y1nq_j, y2nq_j), integration nodes for this integral
+%     h_j_outer, integration weights for this integral
+%     phi_j_outer, vector phi_j evlauated at the integration nodes
+%
+% 2. Integration variables for approximating the integral over Gamma_ell
+% using the product integration method
+%     (y1nq_ell_PIM, y2nq_ell_PIM), integration nodes for this integral
+%     nq_ell_PIM, parameterised 1D integration nodes for this integral
+%     tq_ell_PIM, discretisation on Gamma_ell for integration
+%     h_ell_outer_PIM, intergation weight
+%
+% 3. Integration variables for approximating the integral Psi_ell_r:
+%     (y1nq_j_inner, y2nq_j_inner), intergation nodes in 2D
+%     h_j_inner, integration weights
+%     phi_j_inner, phi_j evaluated at the integration nodes
+%
+% Outputs:
+% f_ell, right hand side evaluated at the collocation points, s.
+
+% First we will compute the leading order behaviour where the collocation
+% points we are evaluating the integral are the quadrature nodes for the
+% outer integral.
+
+ddn_S_ell_j_phij = midpoint_dphikdn_f_diff_screen(k, y1nq_ell_PIM, ...
+    y2nq_ell_PIM, h_j_inner, y1nq_j_inner, y2nq_j_inner, ...
+    phi_j_inner.', norm_j).';
+
+LoB_kernel_uinc = 2*duidn(Gamma_ell, L_ell, k, d, nq_ell_PIM);
+LoB_kernel_beam = ddn_S_ell_j_phij;
+
+% now we will compute the whole vector
+% beam from Gamma_j incident onto Gamma_ell
+S_ell_j_phi_j = midpoint_hankel_f_diff_screen(k, x1s_ell, x2s_ell, ...
+y1nq_j, y2nq_j, h_j_outer, phi_j_outer.');
+
+LoB_uinc =  PIM_int_hankel_f(k, s_ell, h_ell_outer_PIM, ...
+nq_ell_PIM, LoB_kernel_uinc, tq_ell_PIM, C1, C2);
+
+LoB_beam = PIM_int_hankel_f(k, s_ell, h_ell_outer_PIM, ...
+nq_ell_PIM, LoB_kernel_beam, tq_ell_PIM, C1, C2);
+
+f_ell_uinc = incident(k, theta, x1s_ell, x2s_ell) - LoB_uinc;
+
+f_ell_beam = S_ell_j_phi_j - LoB_beam;
+
+f_ell = f_ell_uinc + f_ell_beam;
+
+end
+
