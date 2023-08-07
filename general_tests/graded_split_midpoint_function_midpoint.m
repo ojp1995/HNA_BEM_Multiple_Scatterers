ffcce177bf6fe1d59c07545671a949c6912eef52
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-08-07 13:24:29 +0100

    tests split integral grading method

---
+++ general_tests/graded_split_midpoint_function_midpoint.m
@@ -0,0 +1,21 @@
+% graded split midpoint testing
+
+clear all
+addpath('../General_functions/')
+
+L = 2;
+Lgrad = 0.15*L;
+h = 0.2;
+alpha = 2;
+
+[t_grid, t_mid, w, Q, t_grid_end, t_mid_end, w_end] = ...
+    get_graded_midpoint_quad_points_split(L, Lgrad, ...
+    h, alpha)
+
+figure()
+plot(t_grid, zeros(length(t_grid), 1), '*', 'DisplayName', 'Grid points')
+hold on
+plot(t_mid, zeros(length(t_mid), 1), 'o', 'DisplayName', 'Midpoints')
+plot(t_grid, zeros(length(t_grid), 1), 'k-')
+xlim([0 L])
+legend show
