521830c8dcba58613e9583c0985d6f5290b815f7
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-10-16 19:28:04 +0100

    edits to take into account new grids

--- poly_approx_space/poly_multi_convergence_test.m
+++ poly_approx_space/poly_multi_convergence_test.m
@@ -28,10 +28,12 @@ C2 = pi;
 % run_max = 5;
 
 % quadrature nodes and other information needed
-[G1_data.x_1_q, G1_data.y_1_q, G1_data.x_2_q, G1_data.y_2_q, G1_data.t_grid, G1_data.t_mid, G1_data.w, G1_data.N, G1_data.L] = ...
+[G1_data.x_1_q, G1_data.y_1_q, G1_data.x_2_q, G1_data.y_2_q, ...
+    G1_data.t_grid, G1_data.t_mid_q, G1_data.w, G1_data.N, G1_data.L] = ...
     discretistion_vars_graded(G1_data.G, C_wl, k, Lgrad_coeff, alpha);
 
-[G2_data.x_1_q, G2_data.y_1_q, G2_data.x_2_q, G2_data.y_2_q, G2_data.t_grid, G2_data.t_mid, G2_data.w, G2_data.N, G2_data.L] = ...
+[G2_data.x_1_q, G2_data.y_1_q, G2_data.x_2_q, G2_data.y_2_q, ...
+    G2_data.t_grid, G2_data.t_mid_q, G2_data.w, G2_data.N, G2_data.L] = ...
     discretistion_vars_graded(G2_data.G, C_wl, k, Lgrad_coeff, alpha);
 
 % G1_data.s = [ G1_data.t_mid(1:end) ; flip(G1_data.L - G1_data.t_mid(1:end)) ];
@@ -51,29 +53,55 @@ for j = 1:length(N_bf)
     % The support for the basis functions
 %     C_wl_bf1 = 1/(j+1);
 %     C_wl_bf2 = 1/(j+1);
-    [x1_1, y1_1, x1_2, y1_2, t1_bf_grid, t1_mid, ~, ~] = ...
-    given_N_discretistion_vars_graded(G1_data.G, N_bf(j), Lgrad_coeff, alpha);
-    [x2_1, y2_1, x2_2, y2_2, t2_bf_grid, t2_mid, ~, ~] = ...
-    given_N_discretistion_vars_graded(G2_data.G, N_bf(j), Lgrad_coeff, alpha);
 
-    % collocation points
-    G1_data.s = [ t1_mid ; flip(G1_data.L - t1_mid) ];
-    G1_data.x_col = [ x1_1 ; flip(x1_2) ];
-    G1_data.y_col = [ y1_1 ; flip(y1_2) ];
+    [G1_data.x_1_col, G1_data.y_1_col, G1_data.x_2_col, G1_data.y_2_col,...
+        t1_bf_grid, G1_data.t_mid_col, ~, ~] =  ...
+        given_N_discretistion_vars_graded(G1_data.G, N_bf(j), Lgrad_coeff, alpha);
     
-    % col_choice2 = sort(randi(length(G2_data.t_mid(:)), 20, 1));
-    G2_data.s = [ t2_mid ; flip(G2_data.L - t2_mid) ];
-    G2_data.x_col = [ x2_1 ; flip(x2_2) ];
-    G2_data.y_col = [ y2_1 ; flip(y2_2) ];
-        
+    [G2_data.x_1_col, G2_data.y_1_col, G2_data.x_2_col, G2_data.y_2_col,...
+        t2_bf_grid, G2_data.t_mid_col, ~, ~] = ...
+        given_N_discretistion_vars_graded(G2_data.G, N_bf(j), Lgrad_coeff, alpha);
+
+    G1_data.s = [ G1_data.t_mid_col(1:end) ; flip(G1_data.L - ...
+    G1_data.t_mid_col(1:end)) ];
+    G1_data.x_col = [ G1_data.x_1_col(1:end) ; flip(G1_data.x_2_col(1:end)) ];
+    G1_data.y_col = [ G1_data.y_1_col(1:end) ; flip(G1_data.y_2_col(1:end)) ];
+
+% col_choice2 = sort(randi(length(G2_data.t_mid(:)), 20, 1));
+G2_data.s = [ G2_data.t_mid_col(1:end) ; flip(G2_data.L - ...
+    G2_data.t_mid_col(1:end)) ];
+G2_data.x_col = [ G2_data.x_1_col(1:end) ; flip(G2_data.x_2_col(1:end)) ];
+G2_data.y_col = [ G2_data.y_1_col(1:end) ; flip(G2_data.y_2_col(1:end)) ];
+
+
+
+
+%     [x1_1, y1_1, x1_2, y1_2, t1_bf_grid, t1_mid, ~, ~] = ...
+%     given_N_discretistion_vars_graded(G1_data.G, N_bf(j), Lgrad_coeff, alpha);
+%     [x2_1, y2_1, x2_2, y2_2, t2_bf_grid, t2_mid, ~, ~] = ...
+%     given_N_discretistion_vars_graded(G2_data.G, N_bf(j), Lgrad_coeff, alpha);
+
+%     % collocation points
+%     G1_data.s = [ t1_mid ; flip(G1_data.L - t1_mid) ];
+%     G1_data.x_col = [ x1_1 ; flip(x1_2) ];
+%     G1_data.y_col = [ y1_1 ; flip(y1_2) ];
+%     
+%     % col_choice2 = sort(randi(length(G2_data.t_mid(:)), 20, 1));
+%     G2_data.s = [ t2_mid ; flip(G2_data.L - t2_mid) ];
+%     G2_data.x_col = [ x2_1 ; flip(x2_2) ];
+%     G2_data.y_col = [ y2_1 ; flip(y2_2) ];
+%         
     while length(G1_data.t_grid) < length(t1_bf_grid)  % case where there are more basis function than integration points, rendering the new bf useless
         C_wl= C_wl/2;
         % quadrature nodes and other information needed
-        [G1_data.x_1_q, G1_data.y_1_q, G1_data.x_2_q, G1_data.y_2_q, G1_data.t_grid, G1_data.t_mid, G1_data.w, G1_data.N, G1_data.L] = ...
+        [G1_data.x_1_q, G1_data.y_1_q, G1_data.x_2_q, G1_data.y_2_q, ...
+            G1_data.t_grid, G1_data.t_mid_q, G1_data.w, G1_data.N, G1_data.L] = ...
             discretistion_vars_graded(G1_data.G, C_wl, k, Lgrad_coeff, alpha);
         
-        [G2_data.x_1_q, G2_data.y_1_q, G2_data.x_2_q, G2_data.y_2_q, G2_data.t_grid, G2_data.t_mid, G2_data.w, G2_data.N, G2_data.L] = ...
+        [G2_data.x_1_q, G2_data.y_1_q, G2_data.x_2_q, G2_data.y_2_q, ...
+            G2_data.t_grid, G2_data.t_mid_q, G2_data.w, G2_data.N, G2_data.L] = ...
             discretistion_vars_graded(G2_data.G, C_wl, k, Lgrad_coeff, alpha);
+
     
 %     G1_data.s = [ G1_data.t_mid(1:end) ; flip(G1_data.L - G1_data.t_mid(1:end)) ];
 %     G1_data.x_col = [ G1_data.x_1_q(1:end) ; flip(G1_data.x_2_q(1:end)) ];
@@ -209,7 +237,7 @@ err_phi_1, err_phi_2, EOC_phi_1, EOC_phi_2
 
 toc
 % 
-filename = 'poly_solver_k80_Nbf_10_1280.mat';
-% 
-save(filename, "G1_data", "G2_data", "N_bf", "aj_1", "aj_2", "t1_bf_grid_store", "t2_bf_grid_store")
-% % % 
+% filename = 'poly_solver_k80_Nbf_10_1280.mat';
+% % 
+% save(filename, "G1_data", "G2_data", "N_bf", "aj_1", "aj_2", "t1_bf_grid_store", "t2_bf_grid_store")
+% % % % 
