9df5204f0a33e6cfbe686250f89bebfc1f66ad79
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2024-01-04 11:13:20 +0000

    updates to include plots for errors

--- poly_approx_space/Test1_poly_it.m
+++ poly_approx_space/Test1_poly_it.m
@@ -33,13 +33,44 @@ C2 = pi;
 
 
 % solve
-[G1_data, G2_data, aj_1_R, aj_2_R, us] = ...
+[G1_data, G2_data, aj_1_R, aj_2_R, us, phi_1_r, phi_2_r] = ...
     compute_iteratuve_poly_scattering_prob_2_screens(G1_data, G2_data, ...
     k, Lgrad_coeff, alpha, C_wl_bf1, C_wl_bf2, C_wl_quad, R_max, theta, ...
-    C1, C2, true, false);
+    C1, C2, false, false);
 
-%%
-% compute error wrt to iterations
+%% Plot of relevant iterative solutions on bndy
+it_of_interest = [1, 2, R_max];
+figure()
+for r = 1:length(it_of_interest)
+    txt = ['r = ', mat2str(2*it_of_interest(r) - 2)];
+    plot(G1_data.s(10:end-10)/G1_data.L,...
+        real(phi_1_r(10: end - 10, it_of_interest(r))),...
+        'DisplayName', txt)
+    hold on
+
+end
+legend show
+xlabel('$x/L_{1}$')
+ylabel('$\phi_{1}^{(r)}$')
+title('Iterative approximation to $\phi_{1}$ ')
+xlim([-0.05 1.05])
+
+figure()
+for r = 1:length(it_of_interest)
+    txt = ['r = ', mat2str(2*it_of_interest(r) - 1)];
+    plot(G2_data.s(10:end-10)/G2_data.L, ...
+        real(phi_2_r(10: end - 10, it_of_interest(r))),...
+        'DisplayName', txt)
+    hold on
+
+end
+legend show
+xlabel('$x/L_{2}$')
+ylabel('$\phi_{2}^{(r)}$')
+title('Iterative approximation to $\phi_{2}$ ')
+xlim([-0.05 1.05])
+
+%% compute error wrt to iterations
 C_wl_quad_err = 1/40;
 G1_err_data = get_graded_quad_points(G1_data, C_wl_quad_err, k, ...
     Lgrad_coeff, alpha);
@@ -54,7 +85,7 @@ R_phi1 = [0:2:2*(R_max - 2)];
 R_phi2 = [1:2:2*(R_max - 1)];
 
 figure()
-semilogy(R_phi1, err_L1_G1, 'DisplayName', '$\phi_{1}^{(r)}$')
+semilogy(R_phi1, err_L1_G1, 'DisplayName', '$\phi_{1}^{(r)}$ error')
 hold on
 semilogy(R_phi2, err_L1_G2, 'DisplayName', '$\phi_{2}^{(r)}$ error')
 legend show
