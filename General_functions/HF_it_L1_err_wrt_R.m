7d9cf46343f36e90aecca4d9257d6f170bb7fa4c
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2024-01-18 12:03:42 +0000

    creates function for computing L1 error

---
+++ General_functions/HF_it_L1_err_wrt_R.m
@@ -0,0 +1,10 @@
+function err = HF_it_L1_err_wrt_R(phi_true, phi_test, R_max, err_quad)
+% computing L1 error for HF_it method
+
+err = zeros(R_max-1, 1);
+
+for r = 1:R_max - 1
+
+    err(r) = sum((abs(phi_true - phi_test{r})./abs(phi_true)).*err_quad);
+
+end
