fb5e2da8cb88a15ea39c8b4f99d8dce64b012b4f
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2024-01-18 18:30:21 +0000

    corrects duidn so uses normal externally rather than computing on the fly

--- General_functions/duidn.m
+++ General_functions/duidn.m
@@ -1,4 +1,4 @@
-function z = duidn(G, L, k, d, t)
+function z = duidn(G_data, L, k, d, t)
 % In this function we will be computing the the normal derivative of the
 % incident wave of the screen due to a plane wave incident on it, where the
 % plane wave has vector direction d. We will be evaluating this at the
@@ -14,13 +14,19 @@ function z = duidn(G, L, k, d, t)
 
 % Caution, not sure if this can handle vectors for the x yet.
 
-x1 = G(1, 1) + t.*(G(2, 1) - G(1, 1))/L;
-x2 = G(1, 2) + t.*( G(2, 2) - G(1, 2) )/L;
+% x1 = G(1, 1) + t.*(G(2, 1) - G(1, 1))/L;
+% x2 = G(1, 2) + t.*( G(2, 2) - G(1, 2) )/L;
+
+x1 = G_data.G(1) + t.*(G_data.G(3) - G_data.G(1))/G_data.L;
+x2 = G_data.G(2) + t.*(G_data.G(4) - G_data.G(2))/G_data.L;
 
 
 
 % think there is an error below, should be + on the outside of the term
 % then - infront of d(1)*G(2, 2) ... 
-z = 1i*k*exp(1i*k*( x1*d(1) + x2*d(2)) )*( d(1)*(G(2, 2) - G(1, 2)) ...
-    + d(2)*(G(2, 1) - G(1, 1)) )/L;
+% z = 1i*k*exp(1i*k*( x1*d(1) + x2*d(2)) )*( d(1)*(G(2, 2) - G(1, 2)) ...
+%     + d(2)*(G(2, 1) - G(1, 1)) )/L;
+
+z = 1i*k*( (d(1)*G_data.n(1)*exp(1i*k*x1*d(1))) ...
+    + (d(2)*G_data.n(2)*exp(1i*k*x2*d(2))) );
 
