bef26112672816df1b032a37984d056b7d8c7ccf
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2024-02-05 16:22:19 +0000

    corrects beta for parallel screens

--- General_functions/get_coeff_for_different_sides_of_screen.m
+++ General_functions/get_coeff_for_different_sides_of_screen.m
@@ -45,7 +45,7 @@ if G2_data.G(1) < x_intersect && x_intersect < G2_data.G(3)
 
 elseif m1 == m2  % parallel screen case
 
-    if G2_data.G(1) == G2_data.G(3)  % parallel x
+    if G2_data.G(1) == G2_data.G(3)  % parallel x (similar to | |)
 
         if G2_data.G(2) > G1_data.G(2)  % Gamma2 above Gamma1
             G2_data.beta_outer = ones(length(G2_data.x_q_comb_outer), 1);
@@ -56,14 +56,14 @@ elseif m1 == m2  % parallel screen case
             G2_data.beta_inner = -ones(length(G2_data.x_q_comb_inner), 1);
         end
 
-    elseif G2_data.G(2) == G2_data.G(4)  % parallel y
-        if G2_data.G(1) > G1_data.G(1)  % Gamma2 to the right of (facing front) Gamma1
-            G2_data.beta_outer = ones(length(G2_data.x_q_comb_outer), 1);
-            G2_data.beta_inner = ones(length(G2_data.x_q_comb_inner), 1);
-        else
-            % Gamma2 to the left of (behind) Gamma1
+    elseif G2_data.G(2) == G2_data.G(4)  % parallel y (similar to =)
+        if G2_data.G(2) > G1_data.G(2)  % Gamma2 to the right of (facing front) Gamma1
             G2_data.beta_outer = -ones(length(G2_data.x_q_comb_outer), 1);
             G2_data.beta_inner = -ones(length(G2_data.x_q_comb_inner), 1);
+        else
+            % Gamma2 to the left of (behind) Gamma1
+            G2_data.beta_outer = ones(length(G2_data.x_q_comb_outer), 1);
+            G2_data.beta_inner = ones(length(G2_data.x_q_comb_inner), 1);
         end
 
     else  % case that parallel off angle
